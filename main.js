1b35e5dc72781cab542137a5752747d2adcab082
Timothythebigpro <84496861+Timothythebigpro@users.noreply.github.com>
2021-05-20 15:14:37 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function changecolor() {
+    document.getElementById("braket1").style.color = "green";
+}
+
+function changecolorto() {
+    document.getElementById("braket2").style.color = "blue";
+}
